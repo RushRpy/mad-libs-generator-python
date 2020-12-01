2560232e12d74990d26a39470322149439c12c37
RushRpy <72227914+RushRpy@users.noreply.github.com>
2020-12-01 14:24:14 +0530

    Add files via upload

---
+++ Mad Libs Generator/madlibs.py
@@ -0,0 +1,29 @@
+a_place = input('A Place: ')
+adjective_1 = input('Adjective #1: ')
+adjective_2 = input('Adjective #2: ')
+female_celebrity = input('Female Celebrity: ')
+male_celebrity = input('Male Celebrity: ')
+noun_1 = input('Noun #1: ')
+noun_2 = input('Noun #2: ')
+noun_3 = input('Noun #3: ')
+plural_noun_1 = input('Plural Noun #1: ')
+plural_noun_2 = input('Plural Noun #2: ')
+plural_noun_3 = input('Plural Noun #3: ')
+plural_noun_4 = input('Plural Noun #4: ')
+plural_profession = input('Plural Profession: ')
+
+
+print(f'''Albert Einstein, the son of {male_celebrity} and {female_celebrity}, 
+was born in Ulm, Germany, in 1879. In 1902, he had a job 
+as assistant {noun_1}.in the Swiss patent office and attended
+the University of Zurich. There he began studying atoms, molecules,
+and {plural_noun_1}. He developed the theory of
+{adjective_1} relativity, which expanded the phenomena of sub-atomic
+{plural_noun_2} and {adjective_2} magnetism. In 1921,
+he won the Nobel prize for {plural_noun_3} and was director of
+theoretical physics at the Kaiser Wilhelm {noun_2} in Berlin.
+In 1933, when Hitler became Chancellor of {a_place},
+Einstein came to America to take a post at Princeton Institute for
+{plural_noun_4}, where his theories helped America devise the first
+atomic {noun_3}. There is no question about it: Einstein was
+one of the most brilliant {plural_profession} of our time.''')
